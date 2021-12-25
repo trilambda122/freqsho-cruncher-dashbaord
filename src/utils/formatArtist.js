978a70e5a72b8af6dc9f940a8ef67a3fc23a87eb
Shane Schilling <trilambda122@gmail.com>
2021-12-25 11:19:38 -0600

    minor fixes for artist string

--- src/utils/formatArtist.js
+++ src/utils/formatArtist.js
@@ -1,7 +1,10 @@
 export function fArtist(artistArray) {
   const newString = artistArray.map((i) => i + "").join();
-  const finalString = newString.substring(0, 24) + "...";
-  console.log("STRING:::", newString);
-  return finalString;
-  //   });
+  if (newString.length > 24) {
+    const finalString = newString.substring(0, 24) + "...";
+    console.log("STRING:::", newString);
+    return finalString;
+  } else {
+    return newString;
+  }
 }
